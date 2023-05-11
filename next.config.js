24cd4366562b446952ba46bd69013f130457ad89
WilderDev <williamjohnwilder@outlook.com>
2023-05-11 12:56:33 -0500

    FIX(deploy): CORS

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/(.*)',
+        source: '/(.*)',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
