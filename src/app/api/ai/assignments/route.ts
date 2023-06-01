05622a147314b6773cdfa9bc235b643434362cb3
josephwwilliams <josephwwilliams18@gmail.com>
2023-06-01 03:37:50 -0500

    Cleanup

--- src/app/api/ai/assignments/route.ts
+++ src/app/api/ai/assignments/route.ts
@@ -14,41 +14,6 @@ export async function POST(request: Request) {
     };
   }
 
-  // if (!id || !title || !questions || !lessonPlanContent || !lessonPlanGrade)
-  //   return new Response('Invalid request', { status: 400 });
-
-  // const prompt = generateLessonPlanPrompt({ lesson, teacher, students });
-  // const prompt = `
-  // Create a fill-in-the-blank or multiple choice worksheet for a ${lessonPlanGrade} grade student based on the following lesson content.
-  // Each question should focus on testing the student's knowledge of the concepts from the lesson.
-  // Include a Student Information section, followed by the questions, and conclude with an Answer Key.
-
-  // # Student Information
-  // Name: __________
-  // Date: __________
-
-  // # Lesson Content
-  // ${lessonPlanContent}
-
-  // # Instructions
-  // Create a worksheet with ${questions} well-structured questions that test the student's understanding of the key concepts.
-  // Questions should not focus on the structure or materials used in the lesson, but rather the principles or knowledge shared in the lesson content.
-  // Multiple choice questions should provide 3 incorrect and 1 correct answer options.
-  // Fill-in-the-blank questions should have a single correct answer.
-  // Return the worksheet in clean markdown format.
-
-  // # Example Questions
-  // **Fill-in-the-blank example**: ________ is an important concept from this lesson.
-  // **Multiple-choice example**: Which of the following is related to the key concept of the lesson?
-  // (a) Option 1
-  // (b) Option 2
-  // (c) Option 3
-  // (d) Option 4
-
-  // ${additionalComments && 'Additional guidelines: ' + additionalComments}.
-
-  // After generating the worksheet, create an answer key that provides the correct answers to the questions.
-  // `.trim();
   const { multipleChoiceQuestions, fillInTheBlankQuestions } =
     splitQuestions(questions);
 
